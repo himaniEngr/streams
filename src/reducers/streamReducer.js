10bf728724757601234297bab2193ed19d6e8252
Himani <himani9.engg@gmail.com>
2022-02-02 14:35:17 +0530

    deleting a record and merging list of records

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import {
   FETCH_STREAMS,
   FETCH_STREAM,
@@ -9,13 +10,16 @@ import streams from "../apis/streams";
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapkeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...streams, [action.payload.id]: action.payload };
     case CREATE_STREAM:
       return { ...streams, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...streams, [action.payload.id]: action.payload };
-
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
